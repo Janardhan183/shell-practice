14d72dc3d038c7a4a16c5611f76f76e3b50337a7
Jana Vankam <janamec315@gmail.com>
2026-04-21 12:23:58 +0530

    some message

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -2,3 +2,7 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "Script name: $0"
+echo "Crrent directory: $PWD"
+echo "Who is running: $USER"
+echo "User home diretory: $HOME"
