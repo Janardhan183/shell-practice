9ab82050997d48e91f1a629dc06e1bba1b1369de
Jana Vankam <janamec315@gmail.com>
2026-04-21 08:39:30 +0530

    some message

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# no space  befor and after equal
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you ?"
+echo "$PERSON1:: What are you learning recently"
+echo "$PERSON2:: I'm learning shell scripting $PERSON1"
