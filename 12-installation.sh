37357d6f48e5d164b4761ac1a57885bfabd32898
Jana Vankam <janamec315@gmail.com>
2026-04-22 09:22:12 +0530

    some messages

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Error :: Please runth script with root user"
+    exit 1 #failure is otherthan 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "error :: Installing mysql failed"
+    exit 1
 else
     echo "Installing my sql is success"
 fi
